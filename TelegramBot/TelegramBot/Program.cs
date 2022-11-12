cfdb26ef88ef2c7652b0e6e005588815a0de2a06
Fa1L61 <81235390+Fa1L61@users.noreply.github.com>
2022-11-12 23:53:13 +0300

    Add files via upload

---
+++ TelegramBot/TelegramBot/Program.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace TelegramBot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new TelegramBotClient("5644166395:AAEDMd035mHBIsgeNGodDgIL3X2SOoSD5Jw");
+            client.StartReceiving(Update, Error);
+            Console.ReadLine();
+        }
+
+        private static Task Error(ITelegramBotClient arg1, Exception arg2, CancellationToken arg3)
+        {
+            throw new NotImplementedException();
+        }
+
+        async static Task Update(ITelegramBotClient botClient, Update update, CancellationToken token)
+        {
+            var message = update.Message;
+         
+            Console.WriteLine($"{message.Text} {message.Chat.Username} ({message.Chat.FirstName})");
+            
+            if(message.Text != null)
+            {
+                if (message.Text.ToLower().Contains("привет"))
+                {
+                    await botClient.SendTextMessageAsync(message.Chat.Id, "Здорова, заебал") ;
+                    return; 
+                }
+                else
+                {
+                    await botClient.SendTextMessageAsync(message.Chat.Id, "И чо и чо?");
+                    return;
+                }
+                    
+            }
+           
+        }
+
+       
+    }
+}
